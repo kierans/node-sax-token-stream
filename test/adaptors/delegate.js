2ae3515343802498d3b9fc1899f4e2734eeb751d
Kieran Simpson <kierans777@gmail.com>
2025-08-26 16:07:31 +1000

    Use constant for event name.

--- test/adaptors/delegate.js
+++ test/adaptors/delegate.js
@@ -1,10 +1,11 @@
 "use strict";
 
 import { EventEmitter } from "events";
+import { TokenisingStream } from "tokenising-stream";
 
 export const waitForErrorEvent = (delegate) =>
 	new Promise((resolve, _) => {
-		delegate.once("error", (error) => {
+		delegate.once(TokenisingStream.ERROR_EVENT_NAME, (error) => {
 			resolve(error);
 		});
 	});
